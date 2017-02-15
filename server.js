5fddb03a4cb513b87cbc7daeb68c49ecf88a6c39
Rohal25 <rollykurup25@gmail.com>
2017-02-15 19:46:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-res.send('Article one request is permitted');  
+res.sendFile(path.join(__dirname, 'ui', 'articel-one.html'));
 });
 
 app.get('/article-two',function(req,res){
